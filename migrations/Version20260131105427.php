4b769d73bdf0def9162e5d1ff0a28e630d94108f
seif <smaachaoui@outlook.com>
2026-01-31 11:55:05 +0100

    Migration après la création des entités Genre, Book et Movie

---
+++ migrations/Version20260131105427.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260131105427 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, cover_image VARCHAR(500) DEFAULT NULL, author VARCHAR(255) DEFAULT NULL, publisher VARCHAR(255) DEFAULT NULL, translator VARCHAR(255) DEFAULT NULL, publication_date DATE DEFAULT NULL COMMENT \'(DC2Type:date_immutable)\', genre VARCHAR(100) DEFAULT NULL, page_count INT DEFAULT NULL, isbn VARCHAR(20) DEFAULT NULL, synopsis LONGTEXT DEFAULT NULL, google_books_id VARCHAR(50) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE genre (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, type VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, poster VARCHAR(500) DEFAULT NULL, director VARCHAR(255) DEFAULT NULL, genre VARCHAR(100) DEFAULT NULL, release_date DATE DEFAULT NULL COMMENT \'(DC2Type:date_immutable)\', synopsis LONGTEXT DEFAULT NULL, tmdb_id INT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE genre');
+        $this->addSql('DROP TABLE movie');
+    }
+}
