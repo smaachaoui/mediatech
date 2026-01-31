506d42a95e7471e80329746196bc0413607cc822
seif <smaachaoui@outlook.com>
2026-01-31 20:17:20 +0100

    Correction des noms des routes, après test des routes, les pages sont bien accessible

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('profile/index.html.twig');
     }
 }
