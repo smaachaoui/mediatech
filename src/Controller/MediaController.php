59fa742f86fede3a2a8bc2893bce318633eb22af
seif <smaachaoui@outlook.com>
2026-02-01 15:12:19 +0100

    Modification de mon controlleur pour les appels d'api lors de la recherche des livres et des films

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -2,25 +2,43 @@
 
 namespace App\Controller;
 
+use App\Service\GoogleBooksService;
+use App\Service\TmdbService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class MediaController extends AbstractController
 {
-    #[Route('/books/{id}', name: 'app_book_show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function bookShow(int $id): Response
-    {
+    #[Route('/books/{id}', name: 'app_book_show', methods: ['GET'])]
+    public function bookShow(
+        string $id,
+        GoogleBooksService $googleBooks,
+    ): Response {
+        try {
+            $book = $googleBooks->getById($id);
+        } catch (\Throwable) {
+            throw $this->createNotFoundException('Livre introuvable (Google Books).');
+        }
+
         return $this->render('book/show.html.twig', [
-            'id' => $id,
+            'book' => $book,
         ]);
     }
 
     #[Route('/movies/{id}', name: 'app_movie_show', requirements: ['id' => '\d+'], methods: ['GET'])]
-    public function movieShow(int $id): Response
-    {
+    public function movieShow(
+        int $id,
+        TmdbService $tmdb,
+    ): Response {
+        try {
+            $movie = $tmdb->getById($id);
+        } catch (\Throwable) {
+            throw $this->createNotFoundException('Film introuvable (TMDB).');
+        }
+
         return $this->render('movie/show.html.twig', [
-            'id' => $id,
+            'movie' => $movie,
         ]);
     }
 }
